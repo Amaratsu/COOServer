b2859237247c2053a89112dac8a18099077bb847
Александр <imperaxel@gmail.com>
2021-02-09 11:14:05 +0300

    Added base class Server

---
+++ COO.Server/Data/Models/Server.cs
@@ -0,0 +1,21 @@
+﻿namespace COO.Server.Data.Models
+{
+    using System;
+
+    public class Server
+    {
+        public int Id { get; set; }
+        // 0 - game hub, 1 - open world, 2 - instance
+        public int Type { get; set; }
+        public string IP { get; set; }
+        public string Port { get; set; }
+        public string Name { get; set; }
+        // 0 - on, 1 - off 
+        public int Status { get; set; }
+        // current number of characters
+        public int CNC { get; set; }
+        // maximum number of characters
+        public int MNC { get; set; }
+        public DateTime LastUpdate { get; set; }
+    }
+}
